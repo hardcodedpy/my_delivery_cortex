7d93664c68b5c7f2dad41c02468d228c610c05bf
prime <primecode.adm@gmail.com>
2025-09-23 00:30:24 -0300

    feat: git ignore + unecessary class

--- main.dart
+++ main.dart
@@ -1,17 +1,14 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'bin/data/service/api_server.dart';
 import 'bin/data/service/cli_service.dart';
-import 'bin/server.dart';
+import 'bin/data/service/hive_service.dart';
 import 'bin/services/auth_service.dart';
 
 void main(List<String> args) async {
-  await Server.start();
+  HiveService.init();
 
-  // ================= Loop CLI =================
   print('command:');
-
   final cli = CliService(authService: AuthService());
 
   await for (var line
